0d008c3a12159d3a8cd98e20aba40aeb0009e8b6
Kehinde Ajibade <ajibadekehinde1@gmail.com>
2022-12-14 17:48:12 +0100

    Dynamically added other user data

--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="dashboard-wrapper pa-5">
-    <p class="welcome">Welcome, Kehinde</p>
-    <p class="incoming-currency mb-0">1 united state Dollar equals</p>
+    <p class="welcome">Welcome, {{ name }}</p>
+    <p class="incoming-currency mb-0">1 {{ outgoing }} equals</p>
     <p class="outgoing-currency">
-      {{ $store.state.currency.exchangeRate }} Euro
+      {{ $store.state.currency.exchangeRate }} {{ incoming }}
     </p>
   </div>
 </template>
@@ -11,23 +11,27 @@
 <script>
 // import {mapActions} from vuex;
 export default {
+  data() {
+    return {
+      incoming: '',
+      outgoing: '',
+      name: '',
+    };
+  },
   // methods: {
   //   getCurrencyExchange() {
   //   }
   // },
 
-  methods: {
-    async updateExchangeRate() {
-      await this.$store.dispatch('currency/getExchangeRates');
-    },
-  },
-
   async mounted() {
     window.setInterval(() => {
       this.$store.dispatch('currency/getExchangeRates');
-    }, 10000);
+    }, 30000);
   },
   async created() {
+    this.name = JSON.parse(localStorage.getItem('user')).name;
+    this.incoming = JSON.parse(localStorage.getItem('user')).incomingCurrency;
+    this.outgoing = JSON.parse(localStorage.getItem('user')).outgoingCurrency;
     await this.$store.dispatch('currency/getExchangeRates');
   },
 };
